6651cbc2dd0940ca7988799697383406e73652f9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-29 03:18:03 +0000

    Deploy programming101tutorial/nodejs-tutorial to github.com/programming101tutorial/nodejs-tutorial.git:gh-pages

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit');
+const morgan = require('morgan');
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
+const dotenv = require('dotenv');
+dotenv.config();
+const port = process.env.PORT;
+
+const logger = morgan(':method :url :status :res[content-length] - :response-time ms');
+app.use(logger);
+
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  max: 60, // limit each IP to 60 requests per windowMs
+});
+app.use(limiter);
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.use(function(req, res, next) {
+  res.status(404).send('Sorry can\'t find that!');
+});
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
